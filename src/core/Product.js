8780c3886e2acecbeff4c3a10b47fb02ab35c87a
winaldomanurung <winaldo_satryadi@rocketmail.com>
2022-05-30 10:09:07 +0700

    product show page - related product

--- src/core/Product.js
+++ src/core/Product.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import moment from "moment";
 
-import { read } from "./apiCore";
+import { read, listRelated } from "./apiCore";
 import { useParams } from "react-router-dom";
 import {
   Box,
@@ -18,6 +18,7 @@ import SupportAgentIcon from "@mui/icons-material/SupportAgent";
 import LocalShippingIcon from "@mui/icons-material/LocalShipping";
 import { styled } from "@mui/material/styles";
 import { Chip, Stack } from "@mui/material";
+import ProductCard from "./ProductCard";
 
 import Divider from "@mui/material/Divider";
 
@@ -29,8 +30,10 @@ const TypographyFeature = styled("Typography")(() => ({
 
 const Product = (props) => {
   const [product, setProduct] = useState({});
+  const [relatedProduct, setRelatedProduct] = useState([]);
   const [error, setError] = useState(false);
   const [quantity, setQuantity] = useState(0);
+  const [idChange, setIdChange] = useState("");
 
   const productId = useParams().productId;
 
@@ -40,151 +43,194 @@ const Product = (props) => {
         setError(data.error);
       } else {
         setProduct(data);
+        // fetch related product
+        listRelated(data._id).then((data) => {
+          if (data.error) {
+            setError(data.error);
+          } else {
+            setRelatedProduct(data);
+          }
+        });
       }
     });
   };
 
-  // console.log(product.category.name);
-
   useEffect(() => {
+    setIdChange(productId);
     loadSingleProduct(productId);
-  }, []);
+  }, [productId]);
 
   return (
     <Container
       maxWidth="md"
-      sx={{ display: "flex", flexDirection: "row", marginTop: "100px" }}
+      sx={{ display: "flex", flexDirection: "column", marginTop: "100px" }}
     >
       <Box
         sx={{
           display: "flex",
-          flexDirection: "column",
-          paddingRight: "40px",
-          width: "50%",
+          flexDirection: "row",
         }}
       >
-        <ShowImage item={product} url="product" />
-      </Box>
-      <Box sx={{ display: "flex", flexDirection: "column" }}>
-        <Typography
-          variant="h1"
+        <Box
           sx={{
-            fontSize: "35px",
-            textTransform: "uppercase",
-            fontWeight: "700",
-            marginBottom: "10px",
+            display: "flex",
+            flexDirection: "column",
+            paddingRight: "40px",
+            width: "60%",
           }}
         >
-          {product.name}
-        </Typography>
-        <Stack direction="row" spacing={1}>
-          <Chip
-            label={`Created at ${moment(product.createdAt).fromNow()}`}
-            size="small"
-            sx={{ padding: "0 10px", marginBottom: "20px" }}
-          />
-          <Chip
-            label={`Category: ${
-              product.category ? product.category.name : "None"
-            }`}
-            size="small"
-            sx={{ padding: "0 10px", marginBottom: "20px" }}
-          />
-        </Stack>
-        <Typography
-          variant="h2"
-          sx={{ fontSize: "20px", marginBottom: "10px" }}
-        >
-          {product.description}
-        </Typography>
-
-        <Typography
-          variant="h2"
-          sx={{ fontSize: "20px", fontWeight: "500", marginBottom: "20px" }}
-        >
-          ${product.price}
-        </Typography>
-
-        <Typography
-          sx={{ fontSize: "20px", fontWeight: "500", marginBottom: "20px" }}
-        >
-          Shipment to Estimated delivery
-        </Typography>
-
-        <Stack direction="row" spacing={1}>
-          {product.quantity ? (
-            <Chip
-              label={`In stock: ${product.quantity}`}
-              color="primary"
-              size="small"
-              sx={{ padding: "0 10px", marginBottom: "20px" }}
-            />
-          ) : (
-            <Chip
-              label="Out of stock"
-              size="small"
-              sx={{ padding: "0 10px", marginBottom: "20px" }}
-            />
-          )}
-        </Stack>
-
-        <Box sx={{ maxWidth: "400px", display: "flex", marginBottom: "20px" }}>
-          <TextField
-            size="small"
-            type="number"
-            value={quantity}
-            variant="outlined"
-            inputProps={{
-              min: 1,
-              max: 13,
-              step: 1,
-            }}
-            onChange={(e) => setQuantity(parseInt(e.target.value))}
-            sx={{ maxWidth: "70px", textAlign: "center", paddingRight: "10px" }}
-          />
-          <Button variant="contained">Add to cart</Button>
+          <ShowImage item={product} url="product" />
         </Box>
-        <Divider sx={{ marginBottom: "20px" }} />
+        <Box sx={{ display: "flex", flexDirection: "column" }}>
+          <Typography
+            variant="h1"
+            sx={{
+              fontSize: "35px",
+              textTransform: "uppercase",
+              fontWeight: "700",
+              marginBottom: "10px",
+            }}
+          >
+            {product.name}
+          </Typography>
+          <Stack direction="row" spacing={1}>
+            <Chip
+              label={`Created at ${moment(product.createdAt).fromNow()}`}
+              size="small"
+              sx={{ padding: "0 10px", marginBottom: "20px" }}
+            />
+            <Chip
+              label={`Category: ${
+                product.category ? product.category.name : "None"
+              }`}
+              size="small"
+              sx={{ padding: "0 10px", marginBottom: "20px" }}
+            />
+          </Stack>
+          <Typography
+            variant="h2"
+            sx={{ fontSize: "20px", marginBottom: "10px" }}
+          >
+            {product.description}
+          </Typography>
 
-        <Grid container spacing={2} sx={{ alignSelf: "flex-end" }}>
-          <Grid item xs={6} sx={{ display: "flex", alignItems: "center" }}>
-            <GppGoodIcon fontSize="large" />
-            <Box sx={{ marginLeft: "10px" }}>
-              <TypographyFeature>60-Days Protection</TypographyFeature>
-              <Typography sx={{ fontSize: "14px" }}>
-                Money back guarantee
-              </Typography>
-            </Box>
+          <Typography
+            variant="h2"
+            sx={{ fontSize: "20px", fontWeight: "500", marginBottom: "20px" }}
+          >
+            ${product.price}
+          </Typography>
+
+          <Typography
+            sx={{ fontSize: "20px", fontWeight: "500", marginBottom: "20px" }}
+          >
+            Shipment to Estimated delivery
+          </Typography>
+
+          <Stack direction="row" spacing={1}>
+            {product.quantity ? (
+              <Chip
+                label={`In stock: ${product.quantity}`}
+                color="primary"
+                size="small"
+                sx={{ padding: "0 10px", marginBottom: "20px" }}
+              />
+            ) : (
+              <Chip
+                label="Out of stock"
+                size="small"
+                sx={{ padding: "0 10px", marginBottom: "20px" }}
+              />
+            )}
+          </Stack>
+
+          <Box
+            sx={{ maxWidth: "400px", display: "flex", marginBottom: "20px" }}
+          >
+            <TextField
+              size="small"
+              type="number"
+              value={quantity}
+              variant="outlined"
+              inputProps={{
+                min: 1,
+                max: 13,
+                step: 1,
+              }}
+              onChange={(e) => setQuantity(parseInt(e.target.value))}
+              sx={{
+                maxWidth: "70px",
+                textAlign: "center",
+                paddingRight: "10px",
+              }}
+            />
+            <Button variant="contained">Add to cart</Button>
+          </Box>
+          <Divider sx={{ marginBottom: "20px" }} />
+
+          <Grid container spacing={2} sx={{ alignSelf: "flex-end" }}>
+            <Grid item xs={6} sx={{ display: "flex", alignItems: "center" }}>
+              <GppGoodIcon fontSize="large" />
+              <Box sx={{ marginLeft: "10px" }}>
+                <TypographyFeature>60-Days Protection</TypographyFeature>
+                <Typography sx={{ fontSize: "14px" }}>
+                  Money back guarantee
+                </Typography>
+              </Box>
+            </Grid>
+            <Grid item xs={6} sx={{ display: "flex", alignItems: "center" }}>
+              <PaymentIcon fontSize="large" />
+              <Box sx={{ marginLeft: "10px" }}>
+                <TypographyFeature>Easy Payment</TypographyFeature>
+                <Typography sx={{ fontSize: "14px" }}>
+                  Debit/Credit Cards
+                </Typography>
+              </Box>
+            </Grid>
+            <Grid item xs={6} sx={{ display: "flex", alignItems: "center" }}>
+              <SupportAgentIcon fontSize="large" />
+              <Box sx={{ marginLeft: "10px" }}>
+                <TypographyFeature>Aftersales Support</TypographyFeature>
+                <Typography sx={{ fontSize: "14px" }}>
+                  24/7 product aftersales support
+                </Typography>
+              </Box>
+            </Grid>
+            <Grid item xs={6} sx={{ display: "flex", alignItems: "center" }}>
+              <LocalShippingIcon fontSize="large" />
+              <Box sx={{ marginLeft: "10px" }}>
+                <TypographyFeature>Worldwide Shipment</TypographyFeature>
+                <Typography sx={{ fontSize: "14px" }}>
+                  Shipping all around nations
+                </Typography>
+              </Box>
+            </Grid>
           </Grid>
-          <Grid item xs={6} sx={{ display: "flex", alignItems: "center" }}>
-            <PaymentIcon fontSize="large" />
-            <Box sx={{ marginLeft: "10px" }}>
-              <TypographyFeature>Easy Payment</TypographyFeature>
-              <Typography sx={{ fontSize: "14px" }}>
-                Debit/Credit Cards
-              </Typography>
-            </Box>
-          </Grid>
-          <Grid item xs={6} sx={{ display: "flex", alignItems: "center" }}>
-            <SupportAgentIcon fontSize="large" />
-            <Box sx={{ marginLeft: "10px" }}>
-              <TypographyFeature>Aftersales Support</TypographyFeature>
-              <Typography sx={{ fontSize: "14px" }}>
-                24/7 product aftersales support
-              </Typography>
-            </Box>
-          </Grid>
-          <Grid item xs={6} sx={{ display: "flex", alignItems: "center" }}>
-            <LocalShippingIcon fontSize="large" />
-            <Box sx={{ marginLeft: "10px" }}>
-              <TypographyFeature>Worldwide Shipment</TypographyFeature>
-              <Typography sx={{ fontSize: "14px" }}>
-                Shipping all around nations
-              </Typography>
-            </Box>
-          </Grid>
-        </Grid>
+        </Box>
       </Box>
+      <Divider sx={{ marginBottom: "30px", marginTop: "60px" }}>
+        <Chip
+          sx={{
+            fontSize: "20px",
+            padding: "25px 10px",
+            borderRadius: "50px",
+          }}
+          label="Related product"
+        />
+      </Divider>
+      <Grid
+        container
+        spacing={4}
+        sx={{ justifyContent: "center", marginBottom: "30px" }}
+      >
+        {relatedProduct.map((p, i) => {
+          return (
+            <Grid item xs={4}>
+              <ProductCard key={i} product={p}></ProductCard>
+            </Grid>
+          );
+        })}
+      </Grid>
     </Container>
   );
 };
